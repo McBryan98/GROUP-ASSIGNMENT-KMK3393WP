6395e7b0f692c07ceeebf5f67d557c7640fe5219
65937 <76552155+65937@users.noreply.github.com>
2021-01-22 16:30:16 +0800

    Add files via upload

---
+++ sales.php
@@ -0,0 +1,35 @@
+<?php include('header.php'); ?>
+<body>
+<?php include('navbar.php'); ?>
+<div class="container" style="background-color:#F7E8BB">
+	<h1 class="page-header text-center">SALES</h1>
+	<table class="table table-striped table-bordered">
+		<thead>
+			<th>Date</th>
+			<th>Customer</th>
+			<th>Total Sales</th>
+			<th>Details</th>
+		</thead>
+		<tbody>
+			<?php 
+				$sql="select * from purchase order by purchaseid desc";
+				$query=$conn->query($sql);
+				while($row=$query->fetch_array()){
+					?>
+					<tr>
+						<td><?php echo date('M d, Y h:i A', strtotime($row['date_purchase'])) ?></td>
+						<td><?php echo $row['customer']; ?></td>
+						<td class="text-right">RM<?php echo number_format($row['total'], 2); ?></td>
+						<td><a href="#details<?php echo $row['purchaseid']; ?>" data-toggle="modal" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-search"></span> View </a>
+							<?php include('sales_modal.php'); ?>
+						</td>
+					</tr>
+					<?php
+				}
+			?>
+		</tbody>
+	</table>
+</div>
+</body>
+<footer style="background-color:#A5EEF5"><center>&#169; 2021 | Coded by Sahabat Food Service</center></footer>
+</html>
